7be7df960604aab576d8b10e49888913c4137ff9
Andy <onsubmit@outlook.com>
2024-11-04 20:56:36 -0800

    Move mockFunction to own file

--- src/mockFunction.ts
+++ src/mockFunction.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 export type Func = (...args: any[]) => any;
 export type NormalizedFunc<T extends Func> = (...args: Parameters<T>) => ReturnType<T>;
 
@@ -25,4 +23,72 @@ export interface MockFunction<TFunc extends Func = Func> extends MockFunctionCon
   mockReturnValueOnce(value: ReturnType<TFunc>): this;
   mockResolvedValue(value: Awaited<ReturnType<TFunc>>): this;
   mockResolvedValueOnce(value: Awaited<ReturnType<TFunc>>): this;
+}
+
+export function fn<TFunc extends Func = Func>(callback?: TFunc): MockFunction<TFunc> {
+  const state: Record<string, any> = {};
+  let mockImplementation: NormalizedFunc<TFunc> | undefined;
+  const mockImplementations: Array<NormalizedFunc<TFunc> | undefined> = [];
+
+  const mockInitialValue: MockFunction<TFunc>['mock'] = {
+    calls: [],
+    contexts: [],
+    instances: [],
+    lastCall: undefined,
+    results: [],
+  };
+
+  const mockFn: MockFunction<TFunc> = Object.assign(
+    function (this: MockFunction<TFunc> | unknown, ...args: Parameters<TFunc>) {
+      const { mock } = mockFn;
+
+      state.this = this;
+
+      mock.calls.push(args);
+      mock.contexts.push(this);
+      mock.instances.push(this as MockFunction<TFunc>);
+      mock.lastCall = args;
+
+      const result = (mockImplementations.shift() ?? mockImplementation ?? callback)?.(...args);
+      mock.results.push(result);
+      return result;
+    } as MockFunctionConstructor<TFunc>,
+    {
+      mock: mockInitialValue,
+      state: {},
+      mockClear: () => {
+        const { mock } = mockFn;
+
+        mock.calls.length = 0;
+        mock.contexts.length = 0;
+        mock.instances.length = 0;
+        mock.results.length = 0;
+
+        return mockFn;
+      },
+      mockImplementation: (callback: NormalizedFunc<TFunc>) => {
+        mockImplementation = callback;
+        return mockFn;
+      },
+      mockImplementationOnce: (callback: NormalizedFunc<TFunc>): MockFunction<TFunc> => {
+        mockImplementations.push(callback);
+        return mockFn;
+      },
+      mockReset: () => {
+        mockImplementation = undefined;
+        mockImplementations.length = 0;
+
+        return mockFn.mockClear();
+      },
+      mockReturnThis: () => mockFn.mockImplementation(() => state.this),
+      mockReturnValue: (value: ReturnType<TFunc>) => mockFn.mockImplementation(() => value),
+      mockReturnValueOnce: (value: ReturnType<TFunc>) => mockFn.mockImplementationOnce(() => value),
+      mockResolvedValue: (value: Awaited<ReturnType<TFunc>>) =>
+        mockFn.mockImplementation(() => Promise.resolve(value) as any),
+      mockResolvedValueOnce: (value: Awaited<ReturnType<TFunc>>) =>
+        mockFn.mockImplementationOnce(() => Promise.resolve(value) as any),
+    },
+  );
+
+  return mockFn;
 }
