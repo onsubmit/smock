270ae69dd16dca7413dd9bb5be700ff2c1051999
Andy <onsubmit@outlook.com>
2024-11-05 12:05:38 -0800

    Bind this to object

--- src/spyOn.ts
+++ src/spyOn.ts
@@ -7,7 +7,7 @@ export interface SpyOnFunc {
 export function spyOn<T, K extends keyof T>(object: T, method: K) {
   const objectMethod = object[method];
 
-  const mockFn = fn(objectMethod as Func);
+  const mockFn = fn((objectMethod as Func).bind(object));
   object[method] = mockFn as any;
 
   return mockFn;
